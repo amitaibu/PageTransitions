d07e3c75dc454c191920c0d2bdba5fbdae411c4c
Amitaibu <amitai@gizra.com>
2013-07-18 14:12:28 +0300

    demo.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+
+	$('.pt-page-1').videoBG({
+		position:"fixed",
+		zIndex:0,
+		mp4:'assets/christmas_snow.mp4',
+		ogv:'assets/christmas_snow.ogv',
+		webm:'assets/christmas_snow.webm',
+		poster:'assets/christmas_snow.jpg',
+		opacity:1,
+		fullscreen:true
+	});
+
+
+})
